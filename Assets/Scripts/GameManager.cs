bf04bd464fe9dc2ce156909908ab4c93c5f70284
Guilherme Galarça Dutra <udutra@gmail.com>
2022-04-15 22:59:54 -0300

    UI and fixed bugs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,17 +6,21 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour {
 
     private int score = 0;
+    private int highScore;
     public static GameManager instance;
     public bool gameStarted;
     public GameObject platformSpawner;
-    public GameObject gamePlayUI;
-    public TextMeshProUGUI scoreText;
+    public GameObject gamePlayUI, menuUI;
+    public TextMeshProUGUI scoreText, highScoreText;
     
 
     private void Awake() {
         if (instance == null) {
             instance = this;
         }
+    }
+    private void Start() {
+        GetHighScore();
     }
 
     private void Update() {
@@ -30,12 +34,15 @@ public class GameManager : MonoBehaviour {
     public void GameStart() {
         gameStarted = true;
         platformSpawner.SetActive(true);
+        menuUI.SetActive(false);
         gamePlayUI.SetActive(true);
-        StartCoroutine(UpdaterScore());
+        StartCoroutine("UpdaterScore");
     }
 
     public void GameOver() {
         platformSpawner.SetActive(false);
+        StopCoroutine("UpdaterScore");
+        SaveHighScore();
         Invoke(nameof(ReloadLevel), 1f);
     }
 
@@ -49,5 +56,22 @@ public class GameManager : MonoBehaviour {
             score++;
             scoreText.text = score.ToString();
         }
+    }
+
+    private void SaveHighScore() {
+        if (PlayerPrefs.HasKey("HighScore")) {
+            //we already have a highscore
+            if (score > PlayerPrefs.GetInt("HighScore")) {
+                PlayerPrefs.SetInt("HighScore", score);
+            }
+        }
+        else {
+            //playing for the first time
+            PlayerPrefs.SetInt("HighScore", score);
+        }
+    }
+    private void GetHighScore() {
+        highScore = PlayerPrefs.GetInt("HighScore");
+        highScoreText.text = "BEST SCORE: " + highScore;
     }
 }
